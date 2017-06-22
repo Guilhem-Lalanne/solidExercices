fbf47d401183777506d4669d9cceb0ba85e73929
LALANNE GUILHEM p0606160 <p0606160@ucbl.fr>
2017-06-22 17:50:50 +0200

    New class with a test ment to run class CalculatorTrainer

---
+++ SolidExercices.Tests/CalculatorTrainerTest.cs
@@ -0,0 +1,15 @@
+﻿using NFluent;
+using NUnit.Framework;
+
+namespace SolidExercices.Tests
+{
+    class CalculatorTrainerTest
+    {
+        [Test]
+        public void TestTrainer()
+        {
+            var calculatorTrainer = new CalculatorTrainer();
+            calculatorTrainer.Run();
+        }
+    }
+}
