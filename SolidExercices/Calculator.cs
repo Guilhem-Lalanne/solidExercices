73ef9f8bf446f65354733fb3da3a3088116e35af
LALANNE GUILHEM p0606160 <p0606160@ucbl.fr>
2017-06-22 14:31:06 +0200

    Implement sum in Calculate class : Test is passing.

--- SolidExercices/Calculator.cs
+++ SolidExercices/Calculator.cs
@@ -7,7 +7,18 @@ namespace SolidExercices
     {
         public double Calculate(string operation)
         {
-            throw new NotImplementedException();
+            // split('+')
+            Char op = '+';
+            String[] substrings = operation.Split(op);
+            Double sum = 0;
+            foreach (var substring in substrings)
+            {
+                Double number = Convert.ToDouble(substring);
+                sum = sum + number;
+            }
+
+            return sum;
         }
+
     }
 }
